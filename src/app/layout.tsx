62d175146e9b74c0fd492fa5c45baa43676e244b
Léo Murail <leo.murai85@gmail.com>
2025-04-24 02:21:30 +0200

    layout page loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+//npm
+import { Suspense } from "react";
+
 //fonts
 import { fonts } from "@/lib/fonts";
 
@@ -7,6 +10,7 @@ import "./global.css";
 // ui/components
 import Header from "@/ui/templates/header/header";
 import Footer from "@/ui/templates/footer/footer";
+import PageLoading from "@/ui/components/loading/page-loading/pageLoading";
 
 interface props{
     children: React.ReactNode
@@ -17,9 +21,11 @@ export default async function RootLayout({children}:props){
     <html lang="en">
 
         <body className={fonts.montserrat.className}>
+          <Suspense fallback={<PageLoading/>}>
             <Header />
               {children}
             <Footer />
+          </Suspense>
         </body>
     </html>
   )
